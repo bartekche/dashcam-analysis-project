f3b55de668603ea695836ee951fc95b3122a74a3
Bartosz Chejchman <bartekche@gmail.com>
2024-02-25 22:35:11 +0000

    Fix formatting

--- Dashcam_Ingest/src/com/candidate1064557/dashcam_ingest/DashcamUtilities.java
+++ Dashcam_Ingest/src/com/candidate1064557/dashcam_ingest/DashcamUtilities.java
@@ -9,35 +9,35 @@ package com.candidate1064557.dashcam_ingest;
  * @author candidate1064557
  */
 public class DashcamUtilities {
-    
-    static final double radius  = 6371000.0;
+
+    static final double radius = 6371000.0;
     static final double eps = 0.01;
-    
-    public DashcamUtilities(){
-        
+
+    public DashcamUtilities() {
+
     }
-    
-    private static double toRadians(double ang){
-        return (Math.PI/180D) * ang;
+
+    private static double toRadians(double ang) {
+        return (Math.PI / 180D) * ang;
     }
-    
-    public static double getHaversineDistance(double x1, double y1, double x2, double y2){
+
+    public static double getHaversineDistance(double x1, double y1, double x2, double y2) {
         double deltaLat = toRadians(y2 - y1);
         double deltaLon = toRadians(x2 - x1);
         double lat1 = toRadians(y1);
         double lat2 = toRadians(y2);
-        double hav = Math.sin(deltaLat/2D) * Math.sin(deltaLat/2D) +
-          Math.sin(deltaLon/2D) * Math.sin(deltaLon/2D) * Math.cos(lat1) * Math.cos(lat2); 
+        double hav = Math.sin(deltaLat / 2D) * Math.sin(deltaLat / 2D)
+                + Math.sin(deltaLon / 2D) * Math.sin(deltaLon / 2D) * Math.cos(lat1) * Math.cos(lat2);
         double c = 2D * Math.asin(Math.sqrt(hav));
         return c * radius;
-        
+
     }
-    
-    public static boolean isValid(double x, double y){
+
+    public static boolean isValid(double x, double y) {
         double xAbs = Math.abs(x);
         double yAbs = Math.abs(y);
         boolean xInvalidRange = xAbs >= (180.0 - eps) || xAbs <= eps;
-        boolean yInvalidRange = yAbs >= (90.0 - eps)  || yAbs <= eps;
+        boolean yInvalidRange = yAbs >= (90.0 - eps) || yAbs <= eps;
         return !(xInvalidRange && yInvalidRange);
     }
 }
